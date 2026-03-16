be3930070113298721b41c47af434320bf4cc06b
Roberson <jujuba61777777@gmail>
2026-03-16 11:30:51 -0300

    adicionei nome aluno 5

--- trabalho1.c
+++ trabalho1.c
@@ -4,6 +4,6 @@ int main () {
     printf ("Erick Banhos"), ("idade: 20"), ("RM 572131");
     printf ("Arthur Maziviero"), ("Idade 18"),("RM 573928");
     printf ("Felipe Gallo"), ("idade: 18"), ("RM 569680");
-   
+   printf ("Roberson Junior");
     return 0;
 }
