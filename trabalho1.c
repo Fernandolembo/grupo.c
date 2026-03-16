1366259785db2ba5c711270cab17b5f51061f7e4
Felipe Gallo <galloestudo@gmail.com>
2026-03-16 11:23:31 -0300

    adicionei RM  do aluno 4

--- trabalho1.c
+++ trabalho1.c
@@ -3,7 +3,7 @@ int main () {
     printf ("Fernando"), ("idade: 19"), ("RM 570228");
     printf ("Erick Banhos"), ("idade: 20"), ("RM 572131");
     printf ("Arthur Maziviero"), ("Idade 18"),("RM 573928");
-    printf ("felipe gallo"), ("idade: 18");
+    printf ("Felipe Gallo"), ("idade: 18"), ("RM 569680");
    
     return 0;
 }
