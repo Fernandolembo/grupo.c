1d64b2a2a10cb505e6df29b20fe01f57dda5c191
Felipe Gallo <galloestudo@gmail.com>
2026-03-16 11:22:04 -0300

    adicionei idade do aluno 4

--- trabalho1.c
+++ trabalho1.c
@@ -3,7 +3,7 @@ int main () {
     printf ("Fernando"), ("idade: 19"), ("RM 570228");
     printf ("Erick Banhos"), ("idade: 20"), ("RM 572131");
     printf ("Arthur Maziviero"), ("Idade 18"),("RM 573928");
-    printf ("felipe gallo");
+    printf ("felipe gallo"), ("idade: 18");
    
     return 0;
 }
